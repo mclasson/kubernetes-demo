309ec047e9899de32d523ad5d6d8d770c1822cec
Marcus Classon <marcus.classon@stratiteq.se>
2018-06-01 12:08:33 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # kubernetes-demo
+A few demos for Kubernetes. Run the prerequisites.bat beforehand just to cache images and such.
---
+++ prerequisites.bat
@@ -0,0 +1,13 @@
+
+
+docker pull mclasson/redis:bionic
+docker pull microsoft/dotnet:2.0-runtime
+docker pull microsoft/dotnet:2.0-sdk
+docker pull microsoft/dotnet:2.1-runtime
+docker pull microsoft/dotnet:2.1-sdk
+docker pull microsoft/mssql-server-linux:2017-latest
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install kubernetes-helm -y
+helm init --upgrade --service-account default
+helm repo update
+rem helm search
