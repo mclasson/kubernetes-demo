bae72d4870d50bb02f8028912436896965275740
Marcus Classon <marcus.g.classon@gmail.com>
2018-08-09 14:16:17 +0200

    Added 2.1

--- prerequisites.bat
+++ prerequisites.bat
@@ -3,6 +3,8 @@
 docker pull mclasson/redis:bionic
 docker pull microsoft/dotnet:2.0-runtime
 docker pull microsoft/aspnetcore-build:2.0
+docker pull microsoft/dotnet:2.1-aspnetcore-runtime
+docker pull microsoft/dotnet:2.1-sdk
 docker pull microsoft/aspnetcore:2.0
 docker pull microsoft/dotnet:2.0-sdk
 docker pull microsoft/dotnet:2.1-runtime
